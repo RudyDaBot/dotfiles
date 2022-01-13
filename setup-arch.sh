1ab5ae3c02310c86c482accc6da10e018221c631
Rishab Arora <29125087+grobo021@users.noreply.github.com>
2022-01-13 23:17:02 +0530

    change

--- setup-arch.sh
+++ setup-arch.sh
@@ -7,8 +7,8 @@ sudo pacman -Syu
 sudo sed -i "s/#Color/Color/" /etc/pacman.conf
 sudo sed -i "s/#ParallelDownloads=5/ParallelDownloads=5/" /etc/pacman.conf
 
-# Install Vim, Htop and Git
-sudo pacman -S vim htop git base-devel
+# Install Things 
+sudo pacman -S vim htop git base-devel p7zip
 
 # Install Yay
 git clone https://aur.archlinux.org/yay.git/
@@ -20,21 +20,8 @@ rm -rf yay/
 # Install Virt-Manager with QEMU/KVM
 yay -S qemu virt-manager ebtables
 
-# Get QEMU User credentials
-read -p "Enter username for QEMU user: " qemuUser
-read -sp "Enter password for QEMU user: " qemuPass
-
-# Create QEMU user with a user id less than 1000
-sudo useradd -m -r -p $qemuPass -u 196 $qemuUser
-
-# Add QEMU user to the sudo and libvirt groups
-sudo usermod -aG sudo,libvirt $qemuUser
-
 # Add the person running this script to the libvirt group
 sudo usermod -aG libvirt $USER
-
-# Make QEMU use our new QEMU user
-sudo sed -i "s/#user = \"root\"/user = \"$qemuUser\"/" /etc/libvirt/qemu.conf
 
 # Enable and Start Libvirt Daemon
 sudo systemctl enable libvirtd
@@ -50,18 +37,7 @@ sudo pacman -S github-cli
 sudo pacman -S chromium
 
 # Install Microsoft Fonts
-file=./win.iso
-if [[ -f win.iso ]]
-then
-    sudo pacman -S p7zip
-    7z e win.iso sources/install.wim
-    sudo 7z e install.wim 1/Windows/{Fonts/"*".{ttf,ttc},System32/Licenses/neutral/"*"/"*"/license.rtf} -o/usr/share/fonts/WindowsFonts/
-    rm install.wim
-    sudo chmod 655 /usr/share/fonts/WindowsFonts/
-    fc-cache --force
-else
-    echo "Windows iso not found, skipping microsoft fonts installation. Please make sure a Windows iso with the name 'win.iso' is present in the same directory as this script"
-fi
+./microsoft-fonts-install.sh
 
 # Install Python
 sudo pacman -S python python-pip
