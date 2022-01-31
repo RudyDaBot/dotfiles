d9a879f6ede12b15bad0c1df96a92e7caa9883a3
Rishab Arora <29125087+grobo021@users.noreply.github.com>
2022-01-31 11:59:58 +0530

    edit

--- utils/nvm-install.sh
+++ utils/nvm-install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-nvm_version=`python3 --release utils/get-repo-version.py nvm-sh/nvm`
+nvm_version=`python3 --release get-repo-version.py nvm-sh/nvm`
 url="https://raw.githubusercontent.com/nvm-sh/nvm/${nvm_version}/install.sh"
 
 curl -o- $url | bash
